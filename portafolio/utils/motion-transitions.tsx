80f989abf77b5a0d7c45f331d664ea16031f7083
MiguelAngeloTr <miguelmax248@gmail.com>
2024-06-12 22:25:10 -0500

    navbar

---
+++ portafolio/utils/motion-transitions.tsx
@@ -0,0 +1,16 @@
+export const transitionVariantPage={
+    initial: {
+        x:"100%",
+        width:"100%"
+    },
+    animate:{
+        x:"0%",
+        width:'0%'
+
+    },
+
+    exit:{
+        x:['0%','100%'],
+        width:['0%','100%'],
+    }
+}
